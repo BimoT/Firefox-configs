9eaef1085719f53b2b2ba9e89cc0522b965ddff2
BimoT <91697367+BimoT@users.noreply.github.com>
2022-07-19 10:12:19 +0200

    rewrite of search.js

--- Homepage/Scripts/search.js
+++ Homepage/Scripts/search.js
@@ -1,27 +1,22 @@
 function toggleEngine() {
+  
+  var mySearch = document.getElementById("mySearch");
+  var myInput = document.getElementById("myInput");
+  var textField = document.getElementById("textField");
 
-  var gSearch = document.getElementById("gSearch");
-  var dSearch = document.getElementById("dSearch");
-  var sSearch = document.getElementById("sSearch");
   var logo = document.getElementById("engineLogo").src;
   localStorage.setItem('preferredEngine', logo);
-  // DuckDuckGo -> Google -> Startpage
-  if (logo.includes("DDGlogo.svg")) {
-    document.getElementById("engineLogo").src = "Icons/Googlelogo.svg";
-    gSearch.style.display = "block";
-    dSearch.style.display = "none";
-    sSearch.style.display = "none";
-  } else if (logo.includes("Googlelogo.svg")){
-    document.getElementById("engineLogo").src = "Icons/Startpagelogo.svg";
-    sSearch.style.display = "block";
-    dSearch.style.display = "none";
-    gSearch.style.display = "none";
-  } else {
-    document.getElementById("engineLogo").src = "Icons/DDGlogo.svg";
-    dSearch.style.display = "block";
-    gSearch.style.display = "none";
-    sSearch.style.display = "none";
-  }
+  // DuckDuckGo -> Startpage
 
-  document.getElementById("textField").reset();
+  if (logo.includes("DDGlogo.svg"))
+  {
+    document.getElementById("engineLogo").src = "Icons/Googlelogo.svg";
+    textField.setAttribute("action", "https://google.com/search");
+    myInput.setAttribute("placeholder", "Search with Google");
+  } else
+  {
+    document.getElementById("engineLogo").src = "Icons/DDGlogo.svg";
+    textField.setAttribute("action", "https://duckduckgo.com/");
+    myInput.setAttribute("placeholder", "Search with DuckDuckGo");
+  }
 }
